e664da59f5daef79851ed757e6ad4e089ed90de1
Jack09jk <57107195+Jack09jk@users.noreply.github.com>
2020-01-06 19:51:22 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,8 +38,8 @@ async def help_message_f(client, message):
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"https://t.me/FreeTorrentDownloader/10044)\n\nPlease read the Pinned Message\n\nReply these commands to Magnet link: \n`/leech` \n`/leech archive`\n`/ytdl`\n`/savethumbnail`\n`/clearthumbnail`\n\n If still getting some problems try checking chat or tag and ask admins in chat.\n\n Check out our Channel 
- uploads : LatestGDriveMovies ",
+        f""[Welcome !!](https://t.me/FreeTorrentDownloader/10044)\n\nPlease read the Pinned Message\n\nReply these commands to Magnet link: \n`/leech` \n`/leech archive`\n`/ytdl`\n`/savethumbnail`\n`/clearthumbnail`\n\n If still getting some problems try checking chat or tag and ask admins in chat.\n\n Check out our Channel : @LatestGDriveMovies",
+      
         quote=True
     )
 
